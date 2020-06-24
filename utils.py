b334b23eae04537f94a7ead0bfa040b92b18c54c
r08725022 <cow505285@gmail.com>
2020-06-24 16:12:06 +0800

    add tfidf as feature selection

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 from getNews import getNews
-
+from sklearn.feature_extraction.text import TfidfVectorizer
 def init_args():
 	# you can add any args as you need here
 	parser = ArgumentParser()
@@ -18,9 +18,21 @@ def create_corpus(args):
 		corpus[end_time] =  getNews(args.query,start_time,end_time)
 	return corpus
 def feature_select(corpus,k):
-	#TODO
-	pass
-	return
+	### input params :
+	###   corpus need to  be list of string
+	###   corpus = ["a b c"," c d e"]
+	###   k = return term num
+	### output params:
+	###  ans is list of tuple
+	###  ans = [(doc_id,word,tfidf_val)]
+	###  only return word
+	vectorizer = TfidfVectorizer(sublinear_tf=False, stop_words=None, token_pattern="(?u)\\b\\w+\\b", smooth_idf=True)
+	coo_matrix = vectorizer.fit_transform(corpus).tocoo()
+	features = vectorizer.get_feature_names()
+	vocab = [features[wid] for wid in coo_matrix.col]
+	c_tuples =  zip(coo_matrix.row, vocab, coo_matrix.data)
+	ans = sorted(c_tuples, key=lambda x:x[2],reverse = True)
+	return ans[:k][1]
 def news_select(corpus,k):
 	#TODO
 	pass
