276b993c7cb9af7fb7568e0f352fef4ac9bcd2c4
r08725022 <cow505285@gmail.com>
2020-06-25 14:22:33 +0800

    function can work

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
 		all_corpus = create_corpus(args)
 		for month,m_corpus in all_corpus.items():
 			topk_term,c_tuple = feature_select(m_corpus,k = 20)
+			print("topk_term",topk_term)
 			topk_rel_news = news_select(args.query,m_corpus,c_tuple,k = 3)
 			print("most_rel_term in month:\n%s | most_rel_news:\n%s"%
 				(str(topk_term),topk_rel_news))
--- utils.py
+++ utils.py
@@ -1,12 +1,15 @@
+import numpy as np
 from argparse import ArgumentParser
-from getNews import getNews
+from datetime import datetime as dt
 from sklearn.feature_extraction.text import TfidfVectorizer
+from dateutil.relativedelta import relativedelta
 from getSentSimilarity import get_sent_similarity
+from getNews import getNews
 def init_args():
 	# you can add any args as you need here
 	parser = ArgumentParser()
 	parser.add_argument('--query',default='Trump')
-	parser.add_argument('--time',default='2019/12/10')
+	parser.add_argument('--time',default='12/10/2019')
 	parser.add_argument('--finish',default = False)
 	return parser.parse_args()
 def create_corpus(args):
@@ -14,10 +17,17 @@ def create_corpus(args):
 	## parse args.time into six month
 	for _ in range(6):
 		## start_time and end_time need modify
-		start_time = args.time -  (_+1)* 30
-		end_time = args.time - _*30
+		start_time = subtract_month(args.time,_+1)
+		end_time = subtract_month(args.time,_)
 		corpus[end_time] =  getNews(args.query,start_time,end_time)
 	return corpus
+def subtract_month(time,k):
+	#time = '02/01/2020'
+	dte = dt.strptime(time, '%m/%d/%Y').date()
+	result = dte + relativedelta(months=-k)
+	result.strftime("%m/%d/%Y")
+	return result
+
 def feature_select(corpus,k):
 	### input params :
 	###   corpus need to  be list of string
