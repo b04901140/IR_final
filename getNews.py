962de6a895cc3e63277da7e84492e7412c58db0d
pwlinnn <angus30616@gmail.com>
2020-06-26 18:15:14 +0800

    add get body text

--- getNews.py
+++ getNews.py
@@ -30,6 +30,8 @@ def getNews(topic, start_time, end_time):
         tmp = googlenews.result()
         #result  += [x["title"]+x["desc"] for x in tmp]
         result += get_content(tmp)
+         
+
     return result
 
 
