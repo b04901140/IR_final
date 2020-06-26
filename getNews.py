72ee9e9e283f74438566e9a221c40c313087653c
r08725022 <cow505285@gmail.com>
2020-06-27 00:02:36 +0800

    v1

--- getNews.py
+++ getNews.py
@@ -4,41 +4,39 @@ import requests
 import urllib
 
 def isRedirect(url):
-    print("start")
-    print(url)
-    response = requests.get(url, allow_redirects=True)
-    print(response.url)
-    print('done')
-    if (len(response.history) == 0):
-        return True
-    if response.url != url:
-        return True
-    return False
+	try:
+		response = requests.get(url, allow_redirects=True)
+		if response.url != url:
+			return True
+	except Exception as e:
+		return True
+	return False
 def get_content(result):
-    ret = []
-    for i, news in enumerate(result):
-        url = news['link']
-        if isRedirect(url): continue 
-        response = requests.get(url)
-        article = NewsPlease.from_url(url)
-        if (article.maintext == 'None'): continue
-        else:
-            ret.append(article.title+article.maintext)
-    return ret
+	ret = []
+	for i, news in enumerate(result):
+		url = news['link']
+		if isRedirect(url): continue 
+		response = requests.get(url)
+		if response.status_code != 200: continue
+		article = NewsPlease.from_url(url)
+		if (article.maintext == 'None'): continue
+		else:
+			ret.append(article.title+article.maintext)
+	return ret
 
 def getNews(topic, start_time, end_time):
-    googlenews = GoogleNews(start_time, end_time)
-    result = []
-    for i in range(1,10):
-        googlenews.clear()
-        googlenews.search(topic)
-        googlenews.getpage(i)
-        tmp = googlenews.result()
+	googlenews = GoogleNews(start = start_time, end = end_time)
+	result = []
+	for i in range(1,2):
+		googlenews.clear()
+		googlenews.search(topic)
+		googlenews.getpage(i)
+		tmp = googlenews.result()
 
-        #result  += [x["title"]+x["desc"] for x in tmp]
-        result += get_content(tmp)
-         
+		#result  += [x["title"]+x["desc"] for x in tmp]
+		result += get_content(tmp)
+		 
 
-    return result
+	return result
 
 
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from utils import *
-
+import json
 
 def main():
 	args =  init_args()
 	# all_corpus has six small corpus,is a dict{}
 	all_corpus = create_corpus(args)
-	
+	with open("Trump.json","w") as fp:
+		json.dump(all_corpus,fp)
 	for month,m_corpus in all_corpus.items():
 		topk_term,c_tuple = feature_select(m_corpus,k = 20)
 		topk_rel_news = news_select(args.query,m_corpus,c_tuple,k = 3)
