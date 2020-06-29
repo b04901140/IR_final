495bf6a797fa4b8b8eaddd950520d0887ec9a90e
pwlinnn <angus30616@gmail.com>
2020-06-29 19:07:15 +0800

    modify main

--- main.py
+++ main.py
@@ -14,17 +14,20 @@ def main():
     preprocess_all_corpus(all_corpus)
     print('end preprocess', flush=True)
     #args.query = "vietnam"
+    all_terms = []
     for month,News in all_corpus.items():
         topk_term = feature_select_with_chi2(News,k = 20)
         topk_rel_news = news_select(args.query,News,k = 3)
         #write_to_csv(month,topk_term,topk_rel_news)
         print("month:%s| most_rel_term:\n%s | most_rel_news:\n%s"% (str(month),str(topk_term),topk_rel_news))
+        all_terms.append(topk_term)
         for i in topk_rel_news:
             print('Title: {}\n'.format(News[i].rawTitle))
             print('Link: {}\n'.format(News[i].link))
     #test_result(all_corpus)
 
     while(1):
+        print(all_terms)
         args.query = input('Input a keyword that you are interested in:  ')
         for month,News in all_corpus.items():
             #topk_term = feature_select_with_chi2(News,k = 20)
