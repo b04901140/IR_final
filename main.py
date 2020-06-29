fb7b1089b46528f0c91bada62c160e177970c177
b04901140 <b0401140@ntu.edu.tw>
2020-06-29 20:06:54 +0800

    change output format

--- main.py
+++ main.py
@@ -19,25 +19,29 @@ def main():
         topk_term = feature_select_with_chi2(News,k = 20)
         topk_rel_news = news_select(args.query,News,k = 3)
         #write_to_csv(month,topk_term,topk_rel_news)
-        print("month:%s| most_rel_term:\n%s | most_rel_news:\n%s"% (str(month),str(topk_term),topk_rel_news))
+        print("month:%s| most_rel_term:\n%s"% (str(month),str(topk_term)))
+        print("most_rel_news:")
         all_terms.append(topk_term)
         for i in topk_rel_news:
             print('Title: {}\n'.format(News[i].rawTitle))
-            print('Link: {}\n'.format(News[i].link))
+            print('Link: {}'.format(News[i].link))
+            print('-------------------------------------------------------')
     #test_result(all_corpus)
 
     while(1):
+        print("Followings is all the terms combined from all months")
         print(all_terms)
         args.query = input('Input a keyword that you are interested in:  ')
         for month,News in all_corpus.items():
             #topk_term = feature_select_with_chi2(News,k = 20)
             topk_rel_news = news_select(args.query,News,k = 3)
             #write_to_csv(month,topk_term,topk_rel_news)
-            print("month:%s | most_rel_news:\n%s"% (str(month),topk_rel_news))
+            print("month:%s"% (str(month)))
+            print("most_rel_news:")
             for i in topk_rel_news:
                 print('Title: {}\n'.format(News[i].rawTitle))
-                print('Link: {}\n'.format(News[i].link))
-
+                print('Link: {}'.format(News[i].link))
+                print('-------------------------------------------------------')
     #return
 
 def test_result(all_corpus):
